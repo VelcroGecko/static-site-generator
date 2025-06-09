28b11001fe54386158ae76b63dd9580f076f9218
velcrogecko <charlie.willis12@outlook.com>
2025-06-09 14:48:12 +1000

    pre arch

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -10,6 +10,16 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_diff_testtype(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is another text node", TextType.TEXT, url="Testurl@testing.com")
+        node2 = TextNode("This is another text node", TextType.TEXT,url="Testurl@testing.com")
+        self.assertEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
